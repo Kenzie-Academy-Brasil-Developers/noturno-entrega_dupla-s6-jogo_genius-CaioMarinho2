816fff2d6091f1d7e2b60dd0eefe1e528095a520
Matheus-Moura26 <moura.matheus26@gmail.com>
2022-03-31 18:38:11 -0300

    tudo alterado

--- script.js
+++ script.js
@@ -23,50 +23,51 @@ inferiores.appendChild(botaoAmarelo)
 inferiores.appendChild(botaoAzul)
 
 //TESTE
-const textomeio=document.createElement('p')
-textomeio.innerText='Seja bem vindo'
-textomeio.className='textodomeio'
+const textoMeio=document.createElement('p')
+textoMeio.innerText='Seja bem vindo'
+textoMeio.className='textodomeio'
 const meio= document.getElementById('meio')
-meio.appendChild(textomeio)
+meio.appendChild(textoMeio)
 //TESTE
 
-
-function removerChild(parent,child){
-    parent.removerChild(child)
+function alterarTexto(filho,txt){
+    filho.innerText= txt;
 }
 
- function alterarInserirTexto(parent,filho,txt){
-    parent.removeChild(filho)
-    const child = document.createElement('p')
-    child.innerText= txt
-    parent.appendChild(child)
- }
-
+let array = [1,3,4]
 
 const botaoIniciar = document.createElement('button')
 botaoIniciar.innerText= 'Iniciar'
 botaoIniciar.id = "idBotaoIniciar"
-botaoIniciar.removeId
-botaoIniciar.addEventListener('click', function() {
-    iniciar()
-})
+botaoIniciar.className = 'classeBotao'
+botaoIniciar.addEventListener('click', iniciar)
 meio.appendChild(botaoIniciar)
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // funções do mecanismo de trocar ID
 function padronizarId (numeroBotao){
-    if(numeroBotao === 1){botaoVerde.id= 'greenButton'}
-    if(numeroBotao === 2){botaoVermelho.id= 'redButton'}
-    if(numeroBotao === 3){botaoAmarelo.id= 'yellowButton'}
-    if(numeroBotao === 4){botaoAzul.id= 'blueButton'}
-}
+        if(numeroBotao === 1){  botaoVerde.id= 'greenButton'}
+        if(numeroBotao === 2){  botaoVermelho.id= 'redButton'}
+        if(numeroBotao === 3){  botaoAmarelo.id= 'yellowButton'}
+        if(numeroBotao === 4){  botaoAzul.id= 'blueButton'}
+    }
 
 function analisarSelecionarId (numeroBotao){
-        if(numeroBotao === 1){  botaoVerde.id= 'greenButtonSelect'}
-        if(numeroBotao === 2){  botaoVermelho.id= 'redButtonSelect'}
-        if(numeroBotao === 3){  botaoAmarelo.id= 'yellowButtonSelect'}
-        if(numeroBotao === 4){  botaoAzul.id= 'blueButtonSelect'} 
-    }
+    if(numeroBotao === 1){  botaoVerde.id= 'greenButtonSelect'}
+    if(numeroBotao === 2){  botaoVermelho.id= 'redButtonSelect'}
+    if(numeroBotao === 3){  botaoAmarelo.id= 'yellowButtonSelect'}
+    if(numeroBotao === 4){  botaoAzul.id= 'blueButtonSelect'}
+}
+
+
+function piscar (numeroBotao){
+    setTimeout(()=>{
+        analisarSelecionarId(numeroBotao)},1000);
+
+    setTimeout(()=> {
+        padronizarId(numeroBotao)},2000)
+}
+
 
 function clicaMudarId(numeroBotao){
     botaoVerde.addEventListener('click',analisarSelecionarId(numeroBotao))
@@ -84,108 +85,107 @@ function botoes(){
 maiorPontuacao = 0
 
 //Inicio do jogo
- function iniciar() {
+ function iniciar(churros) {
+console.log(churros)
+    
+    
+     
     //esconder botaoIniciar
     meio.removeChild(botaoIniciar)
 
     //Textos iniciais apagando e reescrevendo
-    /* setTimeout(alterarInserirTexto(meio,textomeio,"Prepare-se e boa sorte"),5000) */
-    setTimeout(()=>{
-        alterarInserirTexto(meio,textomeio,"Prepare-se e boa sorte")
-    },3000)
-    
-
-    
+    setTimeout( ()=> textoMeio.innerText = 'Prepare-se e boa sorte',1500)
     
     
     //txt pedindo atenção aos botões ascendendo
+    setTimeout( ()=> textoMeio.innerText = 'Preste atenção na sequência',1500)
     
-    setTimeout(()=>{
-        alterarInserirTexto(meio,textomeio,"Preste atenção na sequência")
-    },5000)
-
-    placar = 0
-    Valores= []
-    numeroAleatorio = 0
+    
+    
+    
+    
+    let placar = 0
+    let valores= [1,2,3,4]
+    let numeroAleatorio = 0
     
     //jogo começa de fato
     //utilizar um número em cada botão, quando um número daquele botão aparecer ele deverá ascender
-    numeroAleatorio = botoes();
+   /* numeroAleatorio = botoes();
     valores.push(numeroAleatorio);
+     */
+   for(let z = 0; z <= 5; z++){
     
+    
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //Iniciar sequencia dos botões ascendendo todos dentro do array
+    for(let i=0; i < valores.length; i++){
+        
+        setTimeout(()=>{piscar(valores[i])},1000*i)
+        /* ( ()=>analisarSelecionarId(valores[i]),2000 ) */
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        //iniciar sequencia dos botões ascendendo todos dentro do array
-        for(let repeticao2=0; repeticao2 < valores.length; repeticao2++){
-            setTimeout(()=>{
-                analisarSelecionarId(valores[indice1])},5000)
-            
-            setTimeout(()=> {
-                padronizarId(valores[indice1])},5000)
-        }
-
+    }
+        
+    
+    }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         //aparecer txt 'Sua vez! Repita a sequência na ordem correta!'
-        setTimeout(()=>{
-            alterarInserirTexto(meio,textomeio,"Sua vez! Repita a sequência na ordem correta!")
-        },5000)
-
+        setTimeout( ()=> textoMeio.innerText = 'Sua vez! Repita a sequência na ordem correta!',1100*valores.length)
+        
+        
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         //clicar e alterar cor e tamanho baseado no valor passado
-        for(let repeticao3=0; repeticao3 < valores.length; repeticao3++){
-            setTimeout(()=>{
-                clicaMudarId(valores[repeticao3])},5000)
-
-            setTimeout(()=> {
-                padronizarId(valores[indice1])},5000)
-        }
+        let repeticoes = 1
+        /* for(let indice2=0; indice2 < valores.length; indice2++){
+            repeticoes +=1
+            setTimeout(()=> {piscar(valores[indice2])},500*repeticoes)
+        } */
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////        
         //aumentar pontuação a cada acerto 
         if(placar > maiorPontuacao){maiorPontuacao = placar}
 
         //novos numeros aleatórios
-            let numeroAleatorio = botoes()
+            numeroAleatorio = botoes()
         //caso o número do botao clicado seja o mesmo do último ítem valores -> adicionar novo número aleatório em valores
             valores.push(numeroAleatorio)
         
-
-    
     //jogo finaliza
-
-    removeChild(meio,textomeio)
+    // excluir o que existe no meio
+    meio.removeChild(textoMeio)
+    
 
     //mostrar texto 'Que pena'
         const txtFinal1=document.createElement('p')
-        txtFinal1.innerText='Que pena'
-        txtFinal1.className='textodomeio'
+        txtFinal1.innerText='Que pena!'
+        meio.appendChild(txtFinal1)
     //mostrar placar 'Você conseguiu ${placar}'
         const txtFinal2=document.createElement('p')
-        txtFinal2.innerText='Você conseguiu ${placar}'
-        txtFinal2.className='textodomeio'
+        txtFinal2.innerText=`Você conseguiu: ${placar}`
+        txtFinal2.className='textoDoFinal'
+        meio.appendChild(txtFinal2)
     //mostrar maior pontuação 'Maior pontuação: ${maiorPontuacao}'
         const txtFinal3=document.createElement('p')
-        txtFinal3.innerText='Maior pontuação: ${maiorPontuacao}'
-        txtFinal3.className='textodomeio'
+        txtFinal3.innerText=`Maior pontuação: ${maiorPontuacao}`
+        txtFinal3.className='textoDoFinal'
+        meio.appendChild(txtFinal3)
     //mostrar texto 'A sequência está errada. Tente outra vez!'
         const txtFinal4=document.createElement('p')
-        textomeio.innerText='Seja bem vindo'
-        extomeio.className='textodomeio'
+        txtFinal4.innerText='A sequência está errada. Tente outra vez!'
+        txtFinal4.id='sequenciaErrada'
+        txtFinal4.className='textoDoFinal'
+        meio.appendChild(txtFinal4)
     // aparecer botão recomeçar
+    
         const botaoRecomecar = document.createElement('button')
+        botaoRecomecar.className = 'classeBotao'
+        botaoRecomecar.id = 'botaoRecomeçar'
         botaoRecomecar.innerText = 'Recomeçar'
-        botaoRecomecar.addEventListener('click', function() {
+        botaoRecomecar.addEventListener('click', ()=>{
          iniciar()
         })
         meio.appendChild(botaoRecomecar)
-    
-    
-    
-
-
-   
 
     
     
-
+    
 }
 
